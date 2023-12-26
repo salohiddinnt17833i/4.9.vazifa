3184614dda20b597a296a119112ddf5f6176e33c
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2023-12-26 17:53:02 +0500

    1 ta masala bajarildi

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+// ----------3-masala
+// 3. Quyidagi kabi massiv berilganda uning ichidan yoshlarining o'rtachasini topuvchi funksiya yozing
+// let usersAge = [
+//   { name: 'Alice', age: 22, grades: [85, 90, 92, 88] },
+//   { name: 'Bob', age: 20, grades: [78, 85, 80, 88] },
+//   { name: 'Charlie', age: 21, grades: [90, 92, 89, 94] },
+// ];
+// function userAGE(usersAge) {
+//   let counter = 0;
+//   for (let i = 0; i < usersAge.length; i++) {
+//     counter += usersAge[i].age;
+//   }
+//   return counter / usersAge.length;
+// }
+// console.log(userAGE(usersAge));
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
